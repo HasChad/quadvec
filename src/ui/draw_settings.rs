af4b9130a1e8a4109b05ab7245a252754050b11c
HasChad <troll.yahya@gmail.com>
2026-05-02 19:24:00 +0300

    - udpated settings ui to top bar ui for usabilty

--- src/ui/draw_settings.rs
+++ src/ui/draw_settings.rs
@@ -1,4 +1,4 @@
-use egui_macroquad::egui::{self, Align2, Color32, RichText, Slider};
+use egui_macroquad::egui::{self, Color32, RichText, Slider};
 use macroquad::time::get_fps;
 
 use crate::drawing::{DrawState, DrawStyle};
@@ -17,98 +17,87 @@ impl DrawSettings {
     }
 
     pub fn ui(&mut self, ctx: &egui::Context, state: &mut DrawState) {
-        egui::Window::new("")
-            .max_width(100.0)
-            .title_bar(false)
-            .resizable(false)
-            .collapsible(false)
-            .anchor(Align2::LEFT_TOP, (5.0, 5.0))
-            .frame(egui::Frame::window(&egui::Style::default()))
-            .show(ctx, |ui| {
-                egui::Grid::new("poly_grid")
-                    .num_columns(1)
-                    .spacing([5.0, 5.0])
-                    .striped(true)
-                    .show(ui, |ui| {
-                        ui.horizontal(|ui| {
-                            ui.label(RichText::new("Brush Size:").color(Color32::WHITE));
-                            ui.add(
-                                Slider::new(&mut state.brush_size, 1.0..=30.0)
-                                    .trailing_fill(true)
-                                    .step_by(0.1)
-                                    .text_color(Color32::WHITE),
-                            )
-                        });
-                        ui.end_row();
+        egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                // Brush Size
+                ui.label(RichText::new("Brush Size:").color(Color32::WHITE));
+                ui.add(
+                    Slider::new(&mut state.brush_size, 1.0..=30.0)
+                        .trailing_fill(true)
+                        .step_by(0.1)
+                        .text_color(Color32::WHITE),
+                );
+                ui.separator();
 
-                        let mut egui_color = egui::Color32::WHITE;
+                // Brush Color
+                let mut egui_color = egui::Color32::WHITE;
+                egui_color[0] = 255.min((state.brush_color.r * 255.0) as u8);
+                egui_color[1] = 255.min((state.brush_color.g * 255.0) as u8);
+                egui_color[2] = 255.min((state.brush_color.b * 255.0) as u8);
+                egui_color[3] = 255.min((state.brush_color.a * 255.0) as u8);
 
-                        egui_color[0] = 255.min((state.brush_color.r * 255.0) as u8);
-                        egui_color[1] = 255.min((state.brush_color.g * 255.0) as u8);
-                        egui_color[2] = 255.min((state.brush_color.b * 255.0) as u8);
-                        egui_color[3] = 255.min((state.brush_color.a * 255.0) as u8);
+                ui.label(RichText::new("Color:").color(Color32::WHITE));
+                if ui.color_edit_button_srgba(&mut egui_color).changed() {
+                    state.brush_color.a = egui_color.a() as f32 / 255.0;
+                    state.brush_color.r = egui_color.r() as f32 / 255.0;
+                    state.brush_color.g = egui_color.g() as f32 / 255.0;
+                    state.brush_color.b = egui_color.b() as f32 / 255.0;
+                }
+                ui.separator();
 
-                        ui.horizontal(|ui| {
-                            ui.label(RichText::new("Color:").color(Color32::WHITE));
-                            ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                                if ui.color_edit_button_srgba(&mut egui_color).changed() {
-                                    state.brush_color.a = egui_color.a() as f32 / 255.0;
-                                    state.brush_color.r = egui_color.r() as f32 / 255.0;
-                                    state.brush_color.g = egui_color.g() as f32 / 255.0;
-                                    state.brush_color.b = egui_color.b() as f32 / 255.0;
-                                }
-                            })
-                        });
-                        ui.end_row();
+                // Background Color
+                let mut egui_color = egui::Color32::WHITE;
+                egui_color[0] = 255.min((state.bg_color.r * 255.0) as u8);
+                egui_color[1] = 255.min((state.bg_color.g * 255.0) as u8);
+                egui_color[2] = 255.min((state.bg_color.b * 255.0) as u8);
+                egui_color[3] = 255.min((state.bg_color.a * 255.0) as u8);
 
-                        let mut egui_color = egui::Color32::WHITE;
+                ui.label(RichText::new("BG Color:").color(Color32::WHITE));
+                if ui.color_edit_button_srgba(&mut egui_color).changed() {
+                    state.bg_color.a = egui_color.a() as f32 / 255.0;
+                    state.bg_color.r = egui_color.r() as f32 / 255.0;
+                    state.bg_color.g = egui_color.g() as f32 / 255.0;
+                    state.bg_color.b = egui_color.b() as f32 / 255.0;
+                }
+                ui.separator();
 
-                        egui_color[0] = 255.min((state.bg_color.r * 255.0) as u8);
-                        egui_color[1] = 255.min((state.bg_color.g * 255.0) as u8);
-                        egui_color[2] = 255.min((state.bg_color.b * 255.0) as u8);
-                        egui_color[3] = 255.min((state.bg_color.a * 255.0) as u8);
+                if state.style == DrawStyle::Rect
+                    || state.style == DrawStyle::Circle
+                    || state.style == DrawStyle::Ellipse
+                    || state.style == DrawStyle::Poly
+                {
+                    ui.checkbox(&mut state.is_outline, "Outline");
+                    ui.separator();
+                }
 
-                        ui.horizontal(|ui| {
-                            ui.label(RichText::new("BG Color:").color(Color32::WHITE));
-                            ui.with_layout(
-                                egui::Layout::right_to_left(egui::Align::Center),
-                                |ui| {
-                                    if ui.color_edit_button_srgba(&mut egui_color).changed() {
-                                        state.bg_color.a = egui_color.a() as f32 / 255.0;
-                                        state.bg_color.r = egui_color.r() as f32 / 255.0;
-                                        state.bg_color.g = egui_color.g() as f32 / 255.0;
-                                        state.bg_color.b = egui_color.b() as f32 / 255.0;
-                                    }
-                                },
-                            );
-                        });
-                        ui.end_row();
+                // POLYGON SETTINGS
+                if state.style == DrawStyle::Poly {
+                    ui.heading("- Polygon Settings -");
+                    ui.end_row();
 
-                        if state.style == DrawStyle::Poly {
-                            ui.heading("- Polygon Settings -");
-                            ui.end_row();
+                    ui.add(
+                        Slider::new(&mut state.draw_settings.sides, 3..=20)
+                            .trailing_fill(true)
+                            .step_by(0.1)
+                            .text("Edge Count")
+                            .text_color(Color32::WHITE),
+                    );
+                    ui.end_row();
 
-                            ui.add(
-                                Slider::new(&mut state.draw_settings.sides, 3..=20)
-                                    .trailing_fill(true)
-                                    .step_by(0.1)
-                                    .text("Edge Count")
-                                    .text_color(Color32::WHITE),
-                            );
-                            ui.end_row();
+                    ui.add(
+                        Slider::new(&mut state.draw_settings.rotation, 0.0..=360.0)
+                            .trailing_fill(true)
+                            .step_by(0.1)
+                            .text("Rotation")
+                            .text_color(Color32::WHITE),
+                    );
+                    ui.end_row();
+                }
 
-                            ui.add(
-                                Slider::new(&mut state.draw_settings.rotation, 0.0..=360.0)
-                                    .trailing_fill(true)
-                                    .step_by(0.1)
-                                    .text("Rotation")
-                                    .text_color(Color32::WHITE),
-                            );
-                            ui.end_row();
-                        }
-
-                        ui.label(format!("fps: {:.0}", get_fps()));
-                    })
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                    ui.label(RichText::new(format!("fps: {:.0}", get_fps())).color(Color32::WHITE));
+                });
             });
+        });
     }
 }
--- src/ui/style_settings.rs
+++ src/ui/style_settings.rs
@@ -10,42 +10,27 @@ impl StyleSettings {
     }
 
     pub fn ui(&mut self, ctx: &egui::Context, state: &mut DrawState) {
-        egui::Window::new("Options")
+        egui::Window::new("")
             .title_bar(false)
             .resizable(false)
-            .anchor(Align2::RIGHT_TOP, (-5.0, 5.0))
+            .anchor(Align2::RIGHT_TOP, (-5.0, 25.0))
             .show(ctx, |ui| {
-                egui::Grid::new("my_grid")
-                    .num_columns(2)
-                    .spacing([5.0, 5.0])
-                    .min_col_width(60.0)
-                    .striped(true)
-                    .show(ui, |ui| {
-                        ui.radio_value(&mut state.style, DrawStyle::Brush, "Brush");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Line, "Line");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Curve, "Curve");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Arrow, "Arrow");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Rect, "Rect");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Circle, "Circle");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Ellipse, "Ellipse");
-                        ui.end_row();
-                        ui.radio_value(&mut state.style, DrawStyle::Poly, "Poly");
-                        ui.end_row();
-                        if state.style == DrawStyle::Rect
-                            || state.style == DrawStyle::Circle
-                            || state.style == DrawStyle::Ellipse
-                            || state.style == DrawStyle::Poly
-                        {
-                            ui.end_row();
-                            ui.checkbox(&mut state.is_outline, "Outline");
-                        }
-                    })
+                ui.radio_value(&mut state.style, DrawStyle::Brush, "Brush");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Line, "Line");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Curve, "Curve");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Arrow, "Arrow");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Rect, "Rect");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Circle, "Circle");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Ellipse, "Ellipse");
+                ui.end_row();
+                ui.radio_value(&mut state.style, DrawStyle::Poly, "Poly");
+                ui.end_row();
             });
     }
 }
